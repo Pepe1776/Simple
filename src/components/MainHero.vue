7e11658c8ecfe445b86ad630a5cd22a5bb666eec
J Not Query <93418359+Pepe1776@users.noreply.github.com>
2022-07-18 08:24:26 -0700

    m

--- src/components/MainHero.vue
+++ src/components/MainHero.vue
@@ -122,7 +122,7 @@ function Play() {
             cursor: crosshair;
             z-index: 7;
             padding: 50px;
-            left:28%;
+            left:30%;
             top: 75%;
             border-radius: 10px;
             display: flex;
