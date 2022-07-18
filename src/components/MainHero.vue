622269c1f38d6a9468f8f521269fbe82b0763682
J Not Query <93418359+Pepe1776@users.noreply.github.com>
2022-07-17 19:51:28 -0700

    play()

--- src/components/MainHero.vue
+++ src/components/MainHero.vue
@@ -1,23 +1,76 @@
 <template>
     <div>
-        <Transition name="fade">
-            <img class="pulse2" src="../assets/hero.jpeg" alt="hero" />
+        <Transition name="fade" appear>
+            <img v-if="action6" class="pulse2" src="../assets/hero.jpeg" alt="hero" />
         </Transition>
         <div class="black"></div>
         <div class="black0"></div>
-        <Transition name="fade">
+        <Transition name="fade" appear>
             <img class="float" src="../assets/121212.png" alt="hero" />
+        </Transition> 
+            <button v-if="action2" class="btn2" @click="Play(), action2 = !action2">Jump into the future!</button>
+        <Transition name="slide-fade" appear>
+            <p id="action1" class="action1 pulse" v-if="action1">Simulation is rendering .....</p>
+        </Transition>
+        <Transition name="fade" appear>
+            <p id="action1" class="action2" v-if="action3">Welcome to the Future <br> of we do business online</p>
+        </Transition>
+        <Transition name="fade" appear>
+            <img class="img-action" v-if="action5" src="../assets/web3_primer_1.gif" alt="web3_primer_1">
+        </Transition>
+        <Transition name="fade" appear>
+            <p id="action1" class="action2" v-if="action4">We are a team of experts in the field of digitalization</p>
         </Transition>
         <router-link to="/">
             <img src="../assets/Satoshi-logo-300.png" alt="Satoshi logo" class="logo" />
         </router-link>
         <router-link id="call" class="call">
-Hire Me
+            Hire Me
         </router-link>
     </div>
 </template>
 <script lang="ts" setup>
+import { Transition } from "vue";
+import { ref } from "vue";
 
+const action1 = ref(false);
+const action2 = ref(true);
+const action3 = ref(false);
+const action4 = ref(false);
+const action5 = ref(false);
+const action6 = ref(true);
+function Play() {
+    setTimeout(() => {
+        action1.value = true;
+    }, 1000);
+    setTimeout(() => {
+        action6.value = false;
+    }, 500);
+    setTimeout(() => {
+        action1.value = false;
+    }, 5000);
+
+    setTimeout(() => {
+        action3.value = true;
+    }, 7000);
+
+    setTimeout(() => {
+        action5.value = true;
+    }, 7000);
+    setTimeout(() => {
+        action5.value = false;
+    }, 15000);
+
+    setTimeout(() => {
+        action3.value = false;
+    }, 11000);
+    setTimeout(() => {
+        action4.value = true;
+    }, 13000);
+    setTimeout(() => {
+        action4.value = false;
+    }, 17000);
+}
 </script>
 <style scoped>
 @import url('https://fonts.googleapis.com/css2?family=Rubik+Moonrocks&display=swap');
@@ -31,6 +84,21 @@ Hire Me
 .fade-leave-to {
     opacity: 0;
 }
+
+.slide-fade-enter-active {
+    transition: all 0.3s ease-out;
+}
+
+.slide-fade-leave-active {
+    transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+}
+
+.slide-fade-enter-from,
+.slide-fade-leave-to {
+    transform: translateX(20px);
+    opacity: 0;
+}
+
     img {
         width: 100vw;
         height: 100vh;
@@ -40,6 +108,27 @@ Hire Me
         z-index: 3;
         curser: none;
     }
+    .img-action {
+        opacity: 0.2;
+    }
+        .btn2 {
+            font-family: 'Rubik Moonrocks', cursive;
+            color: rgba(51, 255, 0, 0.453);
+            height: 200px;
+            width: auto;
+            font-size: 5rem;
+            cursor: crosshair;
+            z-index: 7;
+            padding: 50px;
+            left:35%;
+            border-radius: 10px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            position: absolute;
+            background: transparent;
+            border: 1px solid rgba(51, 255, 0, 0.453);
+        }
     .black {
         background-color: #000000;
         width: 100vw;
@@ -107,6 +196,40 @@ Hire Me
              opacity: 1;
              transform: scale(1.1);
              color: rgb(51, 255, 0);
+         }
+         .action1 {
+             position: absolute;
+             top: 30%;
+             left: 25%;
+             transform: translate(-50%, -50%);
+             z-index: 3;
+             opacity: 1;
+             width: 50%;
+             height: auto;
+             font-size: 5rem;
+             color: rgba(51, 255, 0, 0.459);
+             font-family: 'Rubik Moonrocks', cursive;
+             transition: all 0.5s ease-in-out;
+             
+         }
+         .action2 {
+             position: absolute;
+             top: 70%;
+             left: 50%;
+             transform: translate(-50%, -50%);
+             z-index: 3;
+             opacity: 1;
+             width: 100%;
+             height: 100%;
+             line-height: normal;
+             font-size: 7rem;
+             color: rgba(51, 255, 0, 0.459);
+             font-family: 'Rubik Moonrocks', cursive;
+             justify-content: center;
+                align-items: center;
+                display: flex;
+                text-shadow: #000000 0px 0px 20px;
+             transition: all 0.5s ease-in-out;
          }
         .pulse {
             animation-name: stretch;
